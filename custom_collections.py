91caa02569b67116b28eb844377aa7b715ceda09
Gushen Ye <y3206771300@outlook.com>
2024-12-25 12:57:50 +0800

    动画第一次测试成功

--- custom_collections.py
+++ custom_collections.py
@@ -6,32 +6,51 @@ import pygame
 import utils
 import game_constants as c
 from game_collections import EventLike, Core, listening, EntityLike, GroupLike
+import os
+
+
+def generate_imageset(path):
+    imageset = {}
+    for i in os.listdir(path):
+        temp = [[], []]
+        for j in os.listdir(os.path.join(path, i)):
+            image = pygame.image.load(os.path.join(path, i, j))
+            image_left = pygame.transform.flip(image, 1, 0)
+            temp[0].append(image)
+            temp[1].append(image_left)
+        imageset[i] = temp
+    return imageset
 
 
 class State:
     def __init__(
-        self, name: str, change_flag=True, loop_flag=True, info: dict = {}
+        self, name: str, change_flag=True, loop_flag=True, info: dict = {}, duration=1
     ) -> None:
         self.name = name
         self.can_be_changed = change_flag
         self.is_loop = loop_flag
+        self.duration = duration
         self.info = info
+
+    @classmethod
+    def create_idle(self):
+        return self("idle", duration=5)
 
 
 class AnimatedSprite(EntityLike):
     def __init__(
         self,
-        imageset: Dict,
+        imageset,
         image: pygame.Surface,
-        state: State = State("idle"),
+        state: State = State.create_idle(),
         direction=0,
     ):
         # direction:1左，0为右
         #
         # state记录当前动画的状态并改变动画
-        super().__init__(self)
         self.image = image
-        self.rect = image.get_rect()
+        rect = image.get_rect()
+        super().__init__(rect=rect)
         # 动画相关
         self.__imageset = imageset
         self.__current_state = state
@@ -40,6 +59,7 @@ class AnimatedSprite(EntityLike):
             self.__direction
         ]
         self.__frame = -1
+        self.__frame_duration_count = -1
         self.__anim_loop_count = -1
 
     @property
@@ -55,21 +75,25 @@ class AnimatedSprite(EntityLike):
             print("The current animation cant be interrputed")
 
     @listening(c.StateEventCode.CHANGE_STATE)
-    def change_state(self, new_state):
+    def change_state(self, event):
         if self.__current_state.can_be_changed:
-            self.state = new_state
+            self.state = event["state"]
             self.__frame = -1
+            self.__frame_duration_count = -1
             self.__anim_loop_count = -1
 
     @listening(c.EventCode.STEP)
-    def step(self):
-        self.__frame += 1
-        if self.__frame % len(self.current_anim) == 0:
+    def step(self, event):
+        self.__frame_duration_count += 1
+        if self.__frame_duration_count % self.state.duration == 0:
+            self.__frame += 1
+            self.__frame_duration_count = 0
+        if self.__frame % len(self.__current_anim) == 0:
             self.__anim_loop_count += 1
             self.__frame = 0
-        if not self.state.is_loop:
+        if not self.state.is_loop and self.__anim_loop_count >= 1:
             event = EventLike(
-                c.StateEventCode.CHANGE_STATE, body={"state": State("idle")}
+                c.StateEventCode.CHANGE_STATE, body={"state": State.create_idle()}
             )
             self.post(event)
         self.image = self.__current_anim[self.__frame]
--- hello_world.py
+++ hello_world.py
@@ -5,33 +5,41 @@
 
 import pygame
 
+from base.constants import EventCode
 import utils
 import game_constants as c
 from game_collections import EventLike, Core, listening, EntityLike, GroupLike
-
+from custom_collections import State, AnimatedSprite, generate_imageset
 
 import pygame
 
 
-class Mob(EntityLike):
-    def __init__(
-        self,
-        rect: pygame.Rect = pygame.Rect(610, 330, 60, 60),  # x, y, width, height
-    ):
-        image = utils.load_image_and_scale(r".\assets\npc\monster\1.png", rect)
-        super().__init__(rect, image=image)
+# class Mob(EntityLike):
+#     def __init__(
+#         self,
+#         rect: pygame.Rect = pygame.Rect(610, 330, 60, 60),  # x, y, width, height
+#     ):
+#         image = utils.load_image_and_scale(r".\assets\npc\monster\1.png", rect)
+#         super().__init__(rect, image=image)
+#
+#     @listening(pygame.KEYDOWN)  # 捕获: 按下按键
+#     def move(self, event: EventLike):
+#         keys = pygame.key.get_pressed()
+#         if keys[pygame.K_w]:  # w被按下
+#             self.rect.y -= 50  # y坐标减少10
+#         if keys[pygame.K_a]:
+#             self.rect.x -= 50
+#         if keys[pygame.K_s]:
+#             self.rect.y += 50
+#         if keys[pygame.K_d]:
+#             self.rect.x += 50
 
-    @listening(pygame.KEYDOWN)  # 捕获: 按下按键
-    def move(self, event: EventLike):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_w]:  # w被按下
-            self.rect.y -= 50  # y坐标减少10
-        if keys[pygame.K_a]:
-            self.rect.x -= 50
-        if keys[pygame.K_s]:
-            self.rect.y += 50
-        if keys[pygame.K_d]:
-            self.rect.x += 50
+
+class Mob(AnimatedSprite):
+    def __init__(self):
+        imageset = generate_imageset("./assets/player/")
+        image = imageset["idle"][0][0]
+        super().__init__(image=image, imageset=imageset)
 
 
 if __name__ == "__main__":
@@ -49,6 +57,9 @@ if __name__ == "__main__":
 
     while True:
         co.window.fill((0, 0, 0))  # 全屏涂黑
+        co.add_event(EventLike.step_event(secord=0))
         for event in co.yield_events():
             group.listen(event)  # 听取: 核心事件队列
+
         co.flip()  # 更新屏幕缓冲区
+        co.tick(60)
